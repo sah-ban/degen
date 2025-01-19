7ca9652ceb6667cc3b70a36f5fa03d2def55c3f6
sahban <sahbaan@proton.me>
2025-01-19 10:57:38 +0530

    updated cover image

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,18 +1,42 @@
+// import { ImageResponse } from "next/og";
+
+// export const alt = "Farcaster Frames V2 Demo";
+// export const size = {
+//   width: 600,
+//   height: 400,
+// };
+
+// export const contentType = "image/png";
+
+// export default async function Image() {
+//   return new ImageResponse(
+//     (
+//       <div tw="h-full w-full flex flex-col justify-center items-center relative bg-white">
+//         <h1 tw="text-3xl">$DEGEN stats by @cashlessman.eth</h1>
+//       </div>
+//     ),
+//     {
+//       ...size,
+//     }
+//   );
+// }
 import { ImageResponse } from "next/og";
 
-export const alt = "Farcaster Frames V2 Demo";
+export const alt = "Demo";
 export const size = {
   width: 600,
   height: 400,
 };
 
 export const contentType = "image/png";
+
 export default async function Image({ params }: { params: { fid: string } }) {
-  const { fid } = params;
+  const { fid } = params; // Extract the `id` parameter from the request context
+
   return new ImageResponse(
     (
       <div tw="h-full w-full flex flex-col justify-center items-center relative bg-white">
-        <h1 tw="text-3xl">hehe {fid}</h1>
+        <h1 tw="text-3xl text-black">Hi, {fid}</h1>
       </div>
     ),
     {
